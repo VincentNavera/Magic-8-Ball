74b0e0690963f4d33557c0f5c57f6e5d2d8b7bae
vincio <vrnavera@protonmail.com>
2021-07-20 03:16:12 +0800

    Initial Commit

---
+++ Magic 8 Ball/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Magic 8 Ball
+//
+//  Created by Vincio on 7/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Magic 8 Ball/Magic_8_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_BallApp.swift
+//  Magic 8 Ball
+//
+//  Created by Vincio on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8_BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
