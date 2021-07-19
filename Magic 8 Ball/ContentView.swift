e63b9a73ce44ed7a47fb04d5a1f9da18e219ed58
vincio <vrnavera@protonmail.com>
2021-07-20 06:20:10 +0800

    added Animations

--- Magic 8 Ball/ContentView.swift
+++ Magic 8 Ball/ContentView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var degrees = 0
+    @State private var move = false
+    @State private var showAnswer = false
+
     var body: some View {
         ZStack{
             Color.black
                 .ignoresSafeArea()
 
             ZStack {
-                RadialGradient(gradient: Gradient(colors: [ .blue ,Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1))]), center: .center, startRadius: 1, endRadius: 200)
+                RadialGradient(gradient: Gradient(colors: [ .blue, Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1))]), center: .center, startRadius: 1, endRadius: 200)
                     .frame(width: 200, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     .clipShape(Triangle())
                     .rotationEffect(.degrees(180))
@@ -25,13 +29,66 @@ struct ContentView: View {
                     .frame(width: 200, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     .rotationEffect(.degrees(180))
                     .blur(radius: 2)
+                if showAnswer {
+                    Text("Text Here".uppercased())
+                        .font(.headline)
+
+                        .fontWeight(.thin)
+                        .frame(width: 100, height: 100, alignment: .top)
+                        .foregroundColor(.yellow)
+                        .shadow(color: .yellow, radius: 5, x: 0.0, y: 0.0)
+                        .blur(radius: 0.4)
+                        .transition(.modifiedTransition)
+
+
+                }
 
 
             }
+            .offset(x: move ? CGFloat(degrees) : 0, y: move ? CGFloat(degrees) : 0)
+            .rotation3DEffect(.degrees(Double(degrees)),
+                              axis: (x: CGFloat((degrees - 5)/10), y: CGFloat((degrees - 5)/10), z: 1),
+                anchor: .center,
+                anchorZ: CGFloat(degrees/100),
+                perspective: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/
+            )
+            .animation(Animation.interpolatingSpring(stiffness: 5, damping: 1)
+            )
 
+
+        }
+        .onTapGesture {
+            degrees += Int.random(in: -20...20)
+                move = true
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    degrees = 0
+                    move = false
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5){
+                        showAnswer = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 15){
+                            showAnswer = false
+
+                        }
+                    }
+
+                }
+
+
+        }
+
+        .onShake {
+            //code here
         }
     }
 }
+
+extension AnyTransition {
+    static var modifiedTransition: AnyTransition {
+        AnyTransition.offset(x: 0, y: 5).combined(with: .opacity)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
