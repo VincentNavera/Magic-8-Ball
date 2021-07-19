f97eb568e13b1c996b5f94cb95dfb1fd85112e46
vincio <vrnavera@protonmail.com>
2021-07-20 07:30:30 +0800

    new background

--- Magic 8 Ball/ContentView.swift
+++ Magic 8 Ball/ContentView.swift
@@ -14,48 +14,62 @@ struct ContentView: View {
     let answers = ["Yes - definitely", "It is decidedly so", "Without a doubt", "Reply hazy, try again", "Ask again later", "Better not tell you now", "My sources say no", "Outlook not so good", "Very doubtful"]
     var body: some View {
         ZStack{
-            Color.black
+            LinearGradient(gradient: Gradient(colors: [.black, .black.opacity(0.9)]), startPoint: .bottomTrailing, endPoint: .topLeading)
                 .ignoresSafeArea()
+            Circle()
+                .fill(.black.opacity(0.3))
+                .frame(width: 300, height: 300, alignment: .center)
+                .offset(y: -15)
+
+
 
             ZStack {
-                RadialGradient(gradient: Gradient(colors: [ .blue, Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1))]), center: .center, startRadius: 1, endRadius: 200)
-                    .frame(width: 200, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .clipShape(Triangle())
-                    .rotationEffect(.degrees(180))
+                ZStack {
+                    RadialGradient(gradient: Gradient(colors: [ .blue, Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1))]), center: .center, startRadius: 1, endRadius: 200)
+                        .frame(width: 200, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .clipShape(Triangle())
+                        .rotationEffect(.degrees(180))
 
 
-                Triangle()
-                    .stroke(Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)).opacity(1), lineWidth: 8)
-                    .frame(width: 200, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .rotationEffect(.degrees(180))
-                    .blur(radius: 2)
-                if showAnswer {
-                    Text(answers[Int.random(in: 0..<answers.count)].uppercased())
-                        .font(.headline)
+                    Triangle()
+                        .stroke(Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)).opacity(1), lineWidth: 8)
+                        .frame(width: 200, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .rotationEffect(.degrees(180))
+                        .blur(radius: 2)
+                    if showAnswer {
+                        Text(answers[Int.random(in: 0..<answers.count)].uppercased())
+                            .font(.headline)
 
-                        .fontWeight(.thin)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 100, height: 100, alignment: .top)
-                        .foregroundColor(.yellow)
-                        .shadow(color: .yellow, radius: 5, x: 0.0, y: 0.0)
-                        .blur(radius: 0.4)
-                        .transition(.modifiedTransition)
-                        .animation(.interpolatingSpring(stiffness: 5, damping: 1))
+                            .fontWeight(.thin)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 100, height: 100, alignment: .top)
+                            .foregroundColor(.yellow)
+                            .shadow(color: .yellow, radius: 5, x: 0.0, y: 0.0)
+                            .blur(radius: 0.4)
+                            .transition(.modifiedTransition)
+                            .animation(.interpolatingSpring(stiffness: 5, damping: 1))
+
+
+                    }
 
 
                 }
-
-
+                .offset(x: move ? CGFloat(degrees) : 0, y: move ? CGFloat(degrees) : 0)
+                .rotation3DEffect(.degrees(Double(degrees)),
+                                  axis: (x: CGFloat((degrees - 5)/10), y: CGFloat((degrees - 5)/10), z: 1),
+                    anchor: .center,
+                    anchorZ: CGFloat(degrees/100),
+                    perspective: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/
+                )
+                .animation(Animation.interpolatingSpring(stiffness: 5, damping: 1)
+            )
             }
-            .offset(x: move ? CGFloat(degrees) : 0, y: move ? CGFloat(degrees) : 0)
-            .rotation3DEffect(.degrees(Double(degrees)),
-                              axis: (x: CGFloat((degrees - 5)/10), y: CGFloat((degrees - 5)/10), z: 1),
-                anchor: .center,
-                anchorZ: CGFloat(degrees/100),
-                perspective: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/
-            )
-            .animation(Animation.interpolatingSpring(stiffness: 5, damping: 1)
-            )
+
+            .frame(width: 300, height: 300, alignment: .center)
+            .overlay(Circle()
+                        .stroke(Color.gray.opacity(0.1), lineWidth: 10)
+                        .offset(y: -15))
+            
 
 
         }
